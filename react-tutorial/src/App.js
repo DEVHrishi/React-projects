1d44722393ea8a2644f43ff8d584d42ccd247318
DEVHrishi <royhrishikesh408@gmail.com>
2022-07-16 22:48:26 +0530

    commit

---
+++ react-tutorial/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { Greet }  from "./components/function";
+
+
+function App() {
+  return (
+    <Greet />
+  )
+}
+
+export default App;
+
+
+

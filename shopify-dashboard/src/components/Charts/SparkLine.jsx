681e2be27ea376d2667f012a4aca31c436c9b06b
Hrishikesh roy <royhrishikesh408@gmail.com>
2022-11-06 23:46:59 +0530

    commit

--- shopify-dashboard/src/components/Charts/SparkLine.jsx
+++ shopify-dashboard/src/components/Charts/SparkLine.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import {SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
-const SparkLine = () => {
+const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
   return (
-    <div>SparkLine</div>
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      boder={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      type={type}
+      xName="x"
+      yName="yval"
+      tooltipSettings={{
+        visible: true,
+        // eslint-disable-next-line no-template-curly-in-string
+        format: '${x} : data ${yval}',
+        trackLineSettings: {
+          visible: true,
+        }
+      }}
+      >
+        <Inject services={[SparklineTooltip]} />
+      </SparklineComponent>
   )
 }
+
+
 
 export default SparkLine

fd8ef769605757a692c5351e889242045e45aa7d
Hrishikesh roy <royhrishikesh408@gmail.com>
2023-04-02 21:09:56 +0530

    commit

--- google-search-app/src/components/Routes.jsx
+++ google-search-app/src/components/Routes.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const Routes = () => {
+import { Routes, Route } from 'react-router-dom'
+import Results from './Results'
+
+const Routess = () => {
   return (
-    <div>Routes</div>
+    <div className='p-4'>
+      <Routes>
+        <Route exact path="/">
+          <Route to="/search" />
+        </Route>
+        <Route exact path={['/search', '/images', '/news', '/videos']}>
+          <Results />
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
-export default Routes
+export default Routess

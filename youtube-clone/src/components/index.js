37915c6335703f03b7bba2da4acf43a2e7a3eb05
Hrishikesh roy <royhrishikesh408@gmail.com>
2023-05-20 23:17:46 +0530

    commit

--- youtube-clone/src/components/index.js
+++ youtube-clone/src/components/index.js
@@ -3,5 +3,6 @@ export { default as Feed } from './Feed';
 export { default as VideoDetails } from './VideoDetails';
 export { default as ChannelDetails } from './ChannelDetails';
 export { default as SearchFeed } from './SearchFeed';
-export { default as SearchBar } from './SearchFeed';
-export { default as Sidebar } from './SearchFeed';
+export { default as SearchBar } from './SearchBar';
+export { default as Sidebar } from './Sidebar';
+export { default as Videos } from './Videos';

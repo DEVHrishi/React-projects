a8f98081007f61c0c47fdc8f90d1b4157cec61d6
DEVHrishi <77627038+DEVHrishi@users.noreply.github.com>
2023-06-05 18:46:27 +0530

    commit

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
 
-import { Navbar, Feed, VideoDetails, ChannelDetails, SearchFeed } from './components';
+import { Navbar, Feed, VideoDetails, ChannelDetail, SearchFeed } from './components';
 
 const App = () => (
     <BrowserRouter>
@@ -10,8 +10,8 @@ const App = () => (
             <Routes>
                 <Route path="/" exact element={<Feed />} />
                 <Route path="/video/:id" element={<VideoDetails />} />
-                <Route path="/channel/:id" element={<ChannelDetails />} />
-                <Route path="/search/:searchItem" element={<SearchFeed />} />
+                <Route path="/channel/:id" element={<ChannelDetail />} />
+                <Route path="/search/:searchTerm" element={<SearchFeed />} />
             </Routes>
         </Box>
     </BrowserRouter>

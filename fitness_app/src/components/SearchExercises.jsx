f61afb6ef403771e31b75b5b16744f17430d41fd
DEVHrishi <77627038+DEVHrishi@users.noreply.github.com>
2023-06-16 23:55:02 +0530

    commit

--- fitness_app/src/components/SearchExercises.jsx
+++ fitness_app/src/components/SearchExercises.jsx
@@ -1,16 +1,17 @@
 import { useState, useEffect } from 'react'
 import { Box, Button, Stack, TextField, Typography } from '@mui/material'
 import { exerciseOptions, fetchData } from '../utils/fetchData'
+import HorizontalScrollbar from './HorizontalScrollbar'
 
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const [search, setSearch] = useState('')
-  const [exercises, setExercises] = useState([])
   const [bodyParts, setBodyParts] = useState([])
 
   useEffect(() => {
     const fetchExercisesData = async () => {
       const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
       setBodyParts(['all', ...bodyPartsData])
+      
     }
     fetchExercisesData();
   }, [])
@@ -71,8 +72,8 @@ const SearchExercises = () => {
         </Button>
       </Box>
 
-      <Box>
-        
+      <Box sx={{ position: 'relative', width: '100%', p: '20px'}}>
+        <HorizontalScrollbar data={bodyParts} bodyPart={bodyPart} setBodyPart={setBodyPart} />
       </Box>
     </Stack>
   )

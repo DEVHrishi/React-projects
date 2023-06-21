0a53ed835f85039750098800f0b79fa228aa8247
DEVHrishi <77627038+DEVHrishi@users.noreply.github.com>
2023-06-21 23:25:04 +0530

    commit

--- fitness_app/src/components/SimilarExercises.jsx
+++ fitness_app/src/components/SimilarExercises.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { Box, Stack, Typography } from "@mui/material"
 
-const SimilarExercises = () => {
+import HorizontalScrollbar from "./HorizontalScrollbar"
+import Loader from "./Loader"
+
+const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
   return (
-    <div>SimilarExercises</div>
+    <Box sx={{ mt: { lg: '100px', xs: '0'}}}>
+      <Typography variant="h3" mb={5}>
+        Exercises that target the same muscle groups
+      </Typography>
+      <Stack>
+        {targetMuscleExercises.length ? <HorizontalScrollbar data={targetMuscleExercises} /> : <Loader />}
+      </Stack>
+
+      <Typography variant="h3" mb={5} mt={5}>
+        Exercises that target the same muscle groups
+      </Typography>
+      <Stack>
+        {equipmentExercises.length ? <HorizontalScrollbar data={equipmentExercises} /> : <Loader />}
+      </Stack>
+    </Box>
   )
 }
 

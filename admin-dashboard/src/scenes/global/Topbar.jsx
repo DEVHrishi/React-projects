d7159d5f9f6a756ea0785f773c3ccbf444abcce1
DEVHrishi <77627038+DEVHrishi@users.noreply.github.com>
2023-06-23 13:02:59 +0530

    commit

---
+++ admin-dashboard/src/scenes/global/Topbar.jsx
@@ -0,0 +1,19 @@
+import { Box, IconButton, useTheme } from '@mui/material'
+import { useContext } from 'react'
+import { ColorModeContext, tokens } from '../../theme'
+import InputBase from '@mui/material'
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import SearchIcon from '@mui/icons-material/Search'
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined'
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined'
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined'
+
+
+const Topbar = () => {
+  return (
+    <div>Topbar</div>
+  )
+}
+
+export default Topbar

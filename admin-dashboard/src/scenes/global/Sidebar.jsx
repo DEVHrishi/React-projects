044c632c1608f929e059dfba9eefc36613216f94
DEVHrishi <77627038+DEVHrishi@users.noreply.github.com>
2023-06-25 23:27:08 +0530

    commit

--- admin-dashboard/src/scenes/global/Sidebar.jsx
+++ admin-dashboard/src/scenes/global/Sidebar.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import { useState } from 'react'
+import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar'
+import { Link } from 'react-router-dom'
+import { Box, IconButton, Typography, useTheme } from '@mui/material'
+import { tokens } from '../../theme'
+
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined'
+import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined'
+import ContactsOutlinedIcon from '@mui/icons-material/ContactsOutlined'
+import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined'
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined'
+import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
+import HelpOutlinedIcon from '@mui/icons-material/HelpOutlined'
+import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined'
+import PieChartOutlinedIcon from '@mui/icons-material/PieChartOutlined'
+import TimelineOutlinedIcon from '@mui/icons-material/TimelineOutlined'
+import MenuOutlinedIcon from '@mui/icons-material/PeopleOutlined'
+import MapOutlinedIcon from '@mui/icons-material/PeopleOutlined'
 
 const Sidebar = () => {
+  const theme = useTheme()
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(false)
+  const [selected, setSelected] = useState('Dashboard')
+
   return (
     <div>Sidebar</div>
   )
--- admin-dashboard/src/scenes/global/Topbar.jsx
+++ admin-dashboard/src/scenes/global/Topbar.jsx
@@ -1,18 +1,72 @@
-import { Box, IconButton, useTheme } from '@mui/material'
+import { Box, IconButton, Paper, useTheme } from '@mui/material'
 import { useContext } from 'react'
 import { ColorModeContext, tokens } from '../../theme'
-import InputBase from '@mui/material'
+import InputBase from '@mui/material/InputBase';
+import Tooltip from '@mui/material/Tooltip';
 import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
-import SearchIcon from '@mui/icons-material/Search'
+import SearchIcon from '@mui/icons-material/Search';
 import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined'
 import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined'
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined'
 
 
 const Topbar = () => {
+  const theme = useTheme()
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext)
+
   return (
-    <div>Topbar</div>
+    <div>
+      <Box display='flex' justifyContent='space-between' p={2}>
+        {/* Search Bar */}
+        <Box
+          backgroundColor={colors.primary[400]}
+          sx={{ p: '2px 4px' , display: 'flex', alignItems: 'center', width: 200 }}
+        >
+          <InputBase
+            sx={{ ml: 2, flex: 1 }}
+            placeholder="Search"
+            inputProps={{ 'aria-label': 'search google maps' }}
+          />
+          <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+            <SearchIcon />
+          </IconButton>
+        </Box>
+
+        {/* Topbar Icons */}
+        <Box display='flex'>
+          <Tooltip title="SetMode">
+            <IconButton onClick={colorMode.toggleColorMode}>
+              {theme.palette.mode === 'dark' ? (
+                <DarkModeOutlinedIcon />
+              ) : (
+                <LightModeOutlinedIcon />
+              )}
+            </IconButton>
+          </Tooltip>
+
+          <Tooltip title="Notification">
+            <IconButton>
+              <NotificationsOutlinedIcon />
+            </IconButton>
+          </Tooltip>
+
+          <Tooltip title="Settings">
+            <IconButton>
+              <SettingsOutlinedIcon />
+            </IconButton>
+          </Tooltip>
+          
+          <Tooltip title="Profile">
+            <IconButton>
+              <PersonOutlinedIcon />
+            </IconButton>
+          </Tooltip>
+        </Box>
+      </Box>
+
+    </div>
   )
 }
 

df94104a626e5d27507d1bb7f18669cfaf4d36cb
DEVHrishi <77627038+DEVHrishi@users.noreply.github.com>
2023-06-27 23:44:59 +0530

    commit

--- admin-dashboard/src/scenes/global/Sidebar.jsx
+++ admin-dashboard/src/scenes/global/Sidebar.jsx
@@ -24,7 +24,78 @@ const Sidebar = () => {
   const [selected, setSelected] = useState('Dashboard')
 
   return (
-    <div>Sidebar</div>
+    <Box
+      sx={{
+        "& .pro-sidebar-inner": {
+          background: `${colors.primary[400]} !important`,
+        },
+        "& .pro-icon-wrapper": {
+          backgroundColor: 'transparent !important',
+        },
+        "& .pro-inner-item": {
+          padding: '5px 35px 5px 20px !important',
+        },
+        "& .pro-inner-item:hover": {
+          color: '#868dfb !important',
+        },
+        "& .pro-menu-item.active": {
+          color: "#6870fa !important",
+        },
+      }}
+    >
+      <ProSidebar collapsed={isCollapsed}>
+        <Menu iconShape="square">
+          {/* LOGO AND MENU ICON */}
+          <MenuItem
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+            style={{
+              margin: "10px 0 20px 0",
+              color: colors.gray[100],
+            }}
+          >
+            {!isCollapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                ml="15px"
+              >
+                <Typography variant="h3" color={colors.gray[100]}>
+                  ADMINIS
+                </Typography>
+                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                  <MenuOutlinedIcon />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+          {/* USER */}
+          {!isCollapsed && (
+            <Box mb='25px'>
+              <Box display='flex' alignItems='center' justifyContent='center'>
+                <img
+                  alt='profile-user'
+                  width='100px'
+                  height='100px'
+                  src={`../../assets/user.jpeg`}
+                  style={{ cursor: 'pointer', borderRadius: '50%' }}
+                />
+              </Box>
+
+              <Box textAlign='center'>
+                <Typography variant='h2' fontWeight='bold' color={colors.gray[100]} sx={{ m: '10px 0 0 0'}}>
+                  Onii-Chan
+                </Typography>
+                <Typography variant='h5' color={colors.greenAccent[500]}>
+                  VP Fancy Admin
+                </Typography>
+              </Box>
+            </Box>
+          )}
+        </Menu>
+      </ProSidebar>
+    </Box>
   )
 }
 
